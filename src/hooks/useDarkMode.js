89ea91047ffba0868507a42ed6a5a59ce450ab92
ianbautista <ianfbautista@gmail.com>
2020-08-05 14:29:00 -0700

    refactored code in index and navBar. MVP done.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,11 +1,7 @@
 import { useLocalStorage } from "./useLocalStorage";
-import { useEffect } from "react";
 
 export function useDarkMode(value) {
-	const [darkMode, setDarkMode] = useLocalStorage(value);
-	useEffect(() => {
-		const body = document.querySelector("body");
-		darkMode ? body.classList.add("dark-mode") : body.classList.remove("dark-mode");
-	}, [darkMode]);
-	return [darkMode, setDarkMode];
+	const [someValue, setSomeValue] = useLocalStorage("darkMode", value);
+
+	return [someValue, setSomeValue];
 }
